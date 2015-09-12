0154c7e419520cb408e8da729db701e21ef7e97f
nemo <git@nemo.ikkoku.de>
2015-09-12 15:04:53 +0200

    Test for the paranoid

--- src/VQVAD/EnergyUtilityTest.java
+++ src/VQVAD/EnergyUtilityTest.java
@@ -38,4 +38,21 @@ public class EnergyUtilityTest {
 		assertEquals(-313.07, res, 0.01);
 	}
 
+	@Test
+	public void testComputeEnergyPerFrame() {
+		DoubleData[] frames = new DoubleData[]{
+			new DoubleData(new double[]{1,2,3,4}),
+			new DoubleData(new double[]{1,1,1,1}),
+			new DoubleData(new double[]{0,0,0,0}),
+		};
+
+		double[] energies = EnergyUtility.computeEnergyPerFrame(frames);
+
+		// 20 * log10(std([1,2,3,4]) + eps) =    2.2185
+		// 20 * log10(std([1,1,1,1]) + eps) = -313.07
+		// 20 * log10(std([0,0,0,0]) + eps) = -313.07
+		assertEquals(2.2185, energies[0], 0.0001);
+		assertEquals(-313.07, energies[1], 0.01);
+		assertEquals(-313.07, energies[2], 0.01);
+	}
 }
